08a5e120392b6568f5fd257b8693bb34b32c78a3
Nirmel Murtic <nirmel.murtic@symphony.is>
2019-05-11 18:52:49 +0200

    - match lineups implementation

--- src/app/lineup/lineup.component.ts
+++ src/app/lineup/lineup.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Lineup} from '../shared/models/lineup';
-import {Player} from '../shared/models/player';
+import {Player, PlayerPosition, PlayerType} from '../shared/models/player';
+
+declare var $: any;
 
 @Component({
   selector: 'app-lineup',
@@ -11,9 +13,7 @@ export class LineupComponent implements OnInit {
 
   _lineup: Lineup;
 
-  public startingPlayers: Map<string, Player[]> = new Map();
-
-  public startingPlayersKeys: IterableIterator<string>;
+  public startingPlayers: Array<Player[]> = [[], [], [], [], [], []];
 
   public playerTypeClasses = [];
 
@@ -22,7 +22,11 @@ export class LineupComponent implements OnInit {
     this._lineup = value;
 
     if(value) {
-      this.populatePlayersMap();
+      this.populatePlayers();
+
+      setTimeout(() => {
+        $('[data-toggle="popover"]').popover();
+      });
     }
   }
 
@@ -35,25 +39,153 @@ export class LineupComponent implements OnInit {
   ngOnInit() {
   }
 
-  public populatePlayersMap() {
-    this.startingPlayers.clear();
+  public populatePlayers() {
+    const getSortCoeficient = (p: Player): number => {
+      if(p.position == PlayerPosition.CF) {
+        return 0;
+      } else if(p.position == PlayerPosition.SS) {
+        return 1;
+      } else if(p.position == PlayerPosition.LW) {
+        return 2;
+      } else if(p.position == PlayerPosition.RW) {
+        return 4;
+      } else if(p.type == PlayerType.STRIKER) {
+        return 3;
+      } else if(p.position == PlayerPosition.AM) {
+        return 5;
+      } else if(p.position == PlayerPosition.DM) {
+        return 9;
+      } else if(p.position == PlayerPosition.LM) {
+        return 6;
+      } else if(p.position == PlayerPosition.RM) {
+        return 8;
+      } else if(p.type == PlayerType.MIDDLE) {
+        return 7;
+      } else if(p.position == PlayerPosition.LB) {
+        return 10;
+      } else if(p.position == PlayerPosition.RB) {
+        return 12;
+      } else if(p.type == PlayerType.DEFENDER) {
+        return 11;
+      } else if(p.type == PlayerType.GOALKEEPER) {
+        return 13;
+      }
+    };
 
-    this.lineup.startingPlayers.forEach(player => {
-      if(!this.startingPlayers.has(player.type)) {
-        this.startingPlayers.set(player.type, []);
+
+    this.startingPlayers = [[], [], [], [], [], []];
+
+    const sorted = this.lineup.startingPlayers.sort((p1, p2) => getSortCoeficient(p2) - getSortCoeficient(p1));
+
+    let formationParts = this.lineupFormation.split("-").filter(
+      value => value.length > 0).map(value => parseInt(value));
+
+    if(formationParts.length > 0) {
+      formationParts.unshift(1);
+
+      let counter = 0;
+
+      for(let i = 0; i < formationParts.length; i++) {
+        for(let j = 0; j < formationParts[i]; j++) {
+          this.startingPlayers[i].push(sorted[counter]);
+
+          counter++;
+        }
+      }
+    } else {
+      const defensiveMiddle = [];
+      const attackingMiddle = [];
+
+      sorted.forEach(player => {
+        switch (player.type) {
+          case 'Goalkeaper':
+          case PlayerType.GOALKEEPER:
+            this.startingPlayers[0].push(player);
+            break;
+          case PlayerType.DEFENDER:
+            this.startingPlayers[1].push(player);
+            break;
+          case PlayerType.MIDDLE:
+            if(player.position == PlayerPosition.DM) {
+              defensiveMiddle.push(player);
+            } else if(player.position == PlayerPosition.AM) {
+              attackingMiddle.push(player);
+            } else {
+              this.startingPlayers[2].push(player);
+            }
+            break;
+          case PlayerType.STRIKER:
+            this.startingPlayers[3].push(player);
+        }
+      });
+
+      if(defensiveMiddle.length > 0) {
+        this.startingPlayers.splice(2, 0, defensiveMiddle);
       }
 
-      this.startingPlayers.get(player.type).push(player);
-    });
+      if(attackingMiddle.length > 0) {
+        this.startingPlayers.splice(this.startingPlayers.length - 3, 0, attackingMiddle);
+      }
+    }
 
-    this.startingPlayersKeys = this.startingPlayers.keys()
+    this.startingPlayers = this.startingPlayers.filter(value => value.length > 0);
+
+    if(this.startingPlayers.length < 5) {
+      this.startingPlayers.splice(2, 0, []);
+      this.startingPlayers.splice(4, 0, []);
+    }
   }
 
-  public get formation() {
+  public get lineupFormation() {
     if(!this.lineup || !this.lineup.formation) {
       return '-';
     }
 
     return this.lineup.formation.replace(/_/g, '-').replace('F-', '');
   }
+
+  public get formation() {
+    if(!this.lineup || !this.lineup.formation) {
+      return this.calculateFormation(this.startingPlayers) + ' (unconfirmed)';
+    }
+
+    return this.lineup.formation.replace(/_/g, '-').replace('F-', '');
+  }
+
+  public calculateFormation(playerLines: Player[][]) : string{
+    let formation = '';
+
+    for(let i = 1; i < playerLines.length; i++) {
+      formation += playerLines[i].length;
+
+      if(i < playerLines.length - 1) {
+        formation += '-';
+      }
+    }
+
+    return formation;
+  }
+
+  public getPlayerInfo(player: Player) {
+    return `
+        <div class="player-info">
+          <div class="row mb-2">
+             <div class="col-12">
+              ${player.fullName}
+             </div>
+          </div>
+          <div class="row">
+             <div class="profile-image">
+              <img src="${player.profilePicture}" class="img-fluid image rounded-circle">
+             </div>
+             <div class="info">
+                <div>Date of birth: <span>${player.birthDate}</span></div>
+                <div>Position: <span>${player.positionName}</span></div>
+                <div>Market value: <span>${player.marketValueRaw}</span></div>
+              </div>
+          </div>
+        </div>
+    `
+  }
 }
+
--- src/app/shared/models/player.ts
+++ src/app/shared/models/player.ts
@@ -8,7 +8,30 @@ export interface Player extends Id {
   marketValueRaw: string;
   profilePicture: string;
   birthDate: Date;
-  position: string;
+  position: PlayerPosition;
   positionName: string;
   type: string;
+}
+
+export enum PlayerType {
+  GOALKEEPER = 'Goalkeeper',
+  DEFENDER = 'Defender',
+  STRIKER = 'Striker',
+  MIDDLE = 'Middle'
+}
+
+export enum PlayerPosition {
+  SS = 'SS',
+  CF = 'CF',
+  CM = 'CM',
+  CB = 'CB',
+  RB = 'RB',
+  LB = 'LB',
+  GK = 'GK',
+  RM = 'RM',
+  LM = 'LM',
+  AM = 'AM',
+  DM = 'DM',
+  LW = 'LW',
+  RW = 'RW'
 }
